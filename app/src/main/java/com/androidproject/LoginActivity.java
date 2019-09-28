f7b21b5cccd2575703014765f46c4b93b969a49d
carloschen1314 <carloschen1314@163.com>
2019-09-28 14:11:35 +0800

    增加登陆界面
    完成登陆界面的UI设计，但未完成：1.跳转至功能界面 2.与后台交互验证信息的功能

--- app/src/main/java/com/androidproject/LoginActivity.java
+++ app/src/main/java/com/androidproject/LoginActivity.java
@@ -2,15 +2,101 @@ package com.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.androidproject.R;
 
+import java.io.File;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editText_accountNumber;
+    private EditText editText_password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
+        editText_accountNumber = (EditText) findViewById(R.id.account_number);
+        editText_password = (EditText) findViewById(R.id.password);
+        login = (Button)findViewById(R.id.login);
+        editText_accountNumber.addTextChangedListener(new JumpTextWatcher());
+        editText_password.addTextChangedListener(new EditLogin());
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                需要传回后台验证的账号密码
+//                Log.d("login",editText_accountNumber.getText().toString());
+//                Log.d("login",editText_password.getText().toString());
+                String accountNumber = editText_accountNumber.getText().toString();
+                String password = editText_accountNumber.getText().toString();
+                //后应补写跳转代码
+            }
+        });
     }
+
+    private class JumpTextWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String str=s.toString();
+            if (str.indexOf("\r")>=0 || str.indexOf("\n")>=0){//发现输入回车符或换行符
+                editText_accountNumber.setText(str.replace("\r","").replace("\n",""));//去掉回车符和换行符
+                editText_password.requestFocus();//让editText2获取焦点
+                editText_password.setSelection(editText_password.getText().length());//若editText2有内容就将光标移动到文本末尾
+            }
+
+        }
+    }
+
+    private class EditLogin implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String str=s.toString();
+            if (str.indexOf("\r")>=0 || str.indexOf("\n")>=0){//发现输入回车符或换行符
+                editText_accountNumber.setText(str.replace("\r","").replace("\n",""));//去掉回车符和换行符
+//                需要传回后台验证的账号密码
+//                Log.d("login",editText_accountNumber.getText().toString());
+//                Log.d("login",editText_password.getText().toString());
+                String accountNumber = editText_accountNumber.getText().toString();
+                String password = editText_accountNumber.getText().toString();
+                //后应补写跳转代码
+            }
+
+        }
+    }
+
 }
