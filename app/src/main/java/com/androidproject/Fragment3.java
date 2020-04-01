cbf851263331ce1300f054578250b12a7af47351
Carlos Chen <55009860+carloschen1314@users.noreply.github.com>
2020-04-01 14:28:59 +0800

    完成fragment3的设计
    完成了群组list的设计，UI设计剩余最后两个模块

--- app/src/main/java/com/androidproject/Fragment3.java
+++ app/src/main/java/com/androidproject/Fragment3.java
@@ -1,6 +1,9 @@
 package com.androidproject;
 
+import android.content.pm.PackageInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +12,17 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Fragment3 extends Fragment {
+
+    private List<group> groupList = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,7 +34,20 @@ public class Fragment3 extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-
+        initgroups();
+        RecyclerView recyclerView = (RecyclerView)getActivity().findViewById(R.id.group_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        groupAdapter adapter = new groupAdapter(groupList);
+        recyclerView.setAdapter(adapter);
     }
 
+    private void initgroups() {
+//        if () {
+//            //设置组信息交互
+//        }
+//        else {
+//            Log.d("Fragment3", "没有找到group");
+//        }
+    }
 }
